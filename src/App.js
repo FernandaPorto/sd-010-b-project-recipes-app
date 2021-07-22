7a239bab7538b99298f249d68f86b3cabd1d4669
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-22 13:04:20 -0300

    Revert "Cloning project from the moment we stoped"
    This reverts commit 034f1a26595044f486eecfec6acbc7a4310b921b.

--- src/App.js
+++ src/App.js
@@ -1,102 +1,21 @@
 import React from 'react';
 import './App.css';
+import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Switch, Route } from 'react-router-dom';
-
-import Pages from './pages/index';
-
-// Source: https://reactrouter.com/web/example/nesting
-
-export default function App() {
-  function renderMainPagesChilds() {
-    return (
-      <Switch>
-        <Route path="/bebidas/:id/in-progress" render={ () => <Pages.InProgress /> } />
-        <Route path="/comidas/:id/in-progress" render={ () => <Pages.InProgress /> } />
-        <Route path="/bebidas/:id" render={ () => <Pages.Details /> } />
-        <Route path="/comidas/:id" render={ () => <Pages.Details /> } />
-        <Route path="/bebidas" render={ () => <Pages.MainPage /> } />
-        <Route path="/comidas" render={ () => <Pages.MainPage /> } />
-      </Switch>
-    );
-  }
-
-  function renderExploreChilds() {
-    return (
-      <Switch>
-        <Route
-          path="/explorar/bebidas/area"
-          render={ () => <Pages.NotFound /> }
-        />
-        <Route
-          path="/explorar/comidas/area"
-          render={ () => <Pages.ExploreArea /> }
-        />
-        <Route
-          path="/explorar/bebidas/ingredientes"
-          render={ () => <Pages.ExploreByIngridients /> }
-        />
-        <Route
-          path="/explorar/comidas/ingredientes"
-          render={ () => <Pages.ExploreByIngridients /> }
-        />
-        <Route
-          path="/explorar/comidas"
-          render={ () => <Pages.ExploreMealsOrDrinks /> }
-        />
-        <Route
-          path="/explorar/bebidas"
-          render={ () => <Pages.ExploreMealsOrDrinks /> }
-        />
-        <Route path="/explorar" render={ () => <Pages.Explore /> } />
-      </Switch>
-    );
-  }
-
-  function renderMotherRotes() {
-    return (
-      <Switch>
-        <Route
-          path="/comidas"
-          render={ renderMainPagesChilds }
-        />
-        <Route
-          path="/bebidas"
-          render={ renderMainPagesChilds }
-        />
-        <Route
-          path="/explorar"
-          render={ renderExploreChilds }
-        />
-        <Route
-          path="/perfil"
-          render={ () => <Pages.Profile /> }
-        />
-        <Route
-          path="/receitas-feitas"
-          render={ () => <Pages.DoneRecipes /> }
-        />
-        <Route
-          path="/receitas-favoritas"
-          render={ () => <Pages.FavoritesRecipes /> }
-        />
-        <Route
-          exact
-          path="/"
-          render={ () => <Pages.Login /> }
-        />
-        <Route
-          path="*"
-          render={ () => <Pages.NotFound /> }
-        />
-      </Switch>
-    );
-  }
-
+function App() {
   return (
     <div className="meals">
-      {renderMotherRotes()}
+      <span className="logo">TRYBE</span>
+      <object
+        className="rocksGlass"
+        type="image/svg+xml"
+        data={ rockGlass }
+      >
+        Glass
+      </object>
     </div>
   );
 }
+
+export default App;
