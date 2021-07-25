35d08193586f163b02851a970f7ccb467223e08a
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-25 20:09:16 -0300

    Css initial ajustments

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -4,21 +4,25 @@ import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
+import '../styles/explore.css';
+
 export default function Explore() {
   return (
-    <>
+    <section className="explore-container">
       <Header />
-      <Link to="/explorar/comidas">
-        <button type="button" data-testid="explore-food">
-          Explorar Comidas
-        </button>
-      </Link>
-      <Link to="/explorar/bebidas">
-        <button type="button" data-testid="explore-drinks">
-          Explorar Bebidas
-        </button>
-      </Link>
+      <div className="explore-btns">
+        <Link to="/explorar/comidas">
+          <button type="button" data-testid="explore-food">
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button type="button" data-testid="explore-drinks">
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
       <Footer />
-    </>
+    </section>
   );
 }
